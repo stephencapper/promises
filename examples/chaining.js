9ef7ca933223fc8b9044c70658a01d2432dcdc78
Stephen Capper <stephencapper@gmail.com>
2024-02-13 15:51:22 -0800

    Complete Bare Minimum Requirements

--- examples/chaining.js
+++ examples/chaining.js
@@ -23,7 +23,7 @@ var addNewUserToDatabaseAsync = function(user) {
     });
 };
 
-addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
+addNewUserToDatabaseAsync({ name: 'Pizza', password: 'chickennuggets' })
   .then(function(savedUser) {
     console.log('All done!');
   })
--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,47 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var fsPromises = require('fs').promises;
+var promisifications = require('./promisification.js');
+var promiseConstructors = require('./promiseConstructor.js');
 
+// promisifications = {
+//   getGitHubProfileAsync: getGitHubProfileAsync,
+//   generateRandomTokenAsync: generateRandomTokenAsync,
+//   readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
+// };
+
+// module.exports = {
+//   getStatusCodeAsync: getStatusCodeAsync,
+//   pluckFirstLineFromFileAsync: pluckFirstLineFromFileAsync
+// };
+
+var writeFilePromise = Promise.promisify((filePath, data, callback) => {
+  fs.writeFile(filePath, data, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
+});
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+
+  return promiseConstructors.pluckFirstLineFromFileAsync(readFilePath)
+    .then((username)=> {
+      return promisifications.getGitHubProfileAsync(username);
+    })
+    .then((profile) => {
+      return writeFilePromise(writeFilePath, JSON.stringify(profile));
+    })
+    .catch((error) => {
+      throw new Error('I am sad');
+    });
+
+  // * (2) then, sends a request to the GitHub API for the user's profile
+  // * (3) then, writes the JSON response of the API to `writeFilePath`
 };
 
 // Export these functions so we can test them
